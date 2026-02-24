135ba807cfc9b6d560dd74160d9ea2d6ddc0eb27
sunulink <infosunulink@gmail.com>
2026-02-24 16:44:20 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+// Imports des pages standards
 import Home from "./pages/Home";
 import AboutPage from "./pages/AboutPage";
 import ServicesPage from "./pages/ServicesPage";
@@ -21,14 +22,17 @@ import FAQPage from "./pages/FAQPage";
 import ContactPage from "./pages/ContactPage";
 import DevisPage from "./pages/DevisPage";
 import NotFound from "./pages/NotFound";
+
+// Imports IA-link-business
 import IAlinkbusiness from "./pages/IA-link-business/IAlinkbusiness";
 import IAlinkDetail from "./pages/IA-link-business/IAlinkDeatil";
 import PackPubIA from "./pages/IA-link-business/PackpubIA";
 import PackpubIADetail from "./pages/IA-link-business/PackpubIADetail";
 
-// Import des pages de réalisations (La principale + les détails)
+// Import des pages de réalisations
 import RealisationsPage from "./pages/RealisationsPage";
 import LogoIdentitePage from "./pages/LogoIdentitePage";
+import AffichesSupportsPage from "./pages/AffichesSupportsPage";
 
 const queryClient = new QueryClient();
 
@@ -49,28 +53,37 @@ const App = () => {
         <Sonner />
         <BrowserRouter>
           <Routes>
+            {/* Pages Principales */}
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/services" element={<ServicesPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/devis" element={<DevisPage />} />
+            <Route path="/faq" element={<FAQPage />} />
+
+            {/* Ressources & Blog */}
             <Route path="/ressources" element={<ResourcesPage />} />
             <Route path="/ressources/:slug" element={<ResourceDetailPage />} />
             <Route path="/article/:articleSlug" element={<ArticlePage />} />
-            <Route path="/boost-my-pub" element={<BoostMyPubPage />} />
-            <Route path="/boost-my-pub/:packSlug" element={<BoostMyPubDetailPage />} />
             <Route path="/blog" element={<BlogPage />} />
             <Route path="/blog/:slug" element={<BlogDetailPage />} />
-            <Route path="/faq" element={<FAQPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/devis" element={<DevisPage />} />
+
+            {/* Boost My Pub */}
+            <Route path="/boost-my-pub" element={<BoostMyPubPage />} />
+            <Route path="/boost-my-pub/:packSlug" element={<BoostMyPubDetailPage />} />
+
+            {/* IA Link Business */}
             <Route path="/ialinkbusiness" element={<IAlinkbusiness />} />
             <Route path="/ialinkbusiness/:serviceTitle" element={<IAlinkDetail />} />
             <Route path="/pack-pub-ia" element={<PackPubIA />} />
             <Route path="/pack-pub-ia/:packSlugia" element={<PackpubIADetail />} />
 
-            {/* Routes pour les Réalisations */}
+            {/* --- SECTION RÉALISATIONS --- */}
             <Route path="/realisations" element={<RealisationsPage />} />
             <Route path="/realisations/logo-identite" element={<LogoIdentitePage />} />
+            <Route path="/realisations/affiches-supports" element={<AffichesSupportsPage />} />
 
+            {/* 404 - Page non trouvée */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
