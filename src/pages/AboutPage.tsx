88669092677467365c9b46e9373e74cbad4d695e
sunulink <infosunulink@gmail.com>
2026-07-23 22:35:16 +0000

    Update AboutPage.tsx
    A Propos

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -142,7 +142,10 @@ const AboutPage = () => {
             </h2>
             <div className="max-w-4xl mx-auto space-y-6">
               <p className="text-xl md:text-2xl text-gray-700 leading-relaxed" data-aos="fade-up">
-                <strong className="text-sunuBlue">SunuLink Consulting</strong> accompagne les organisations qui veulent transformer leur visibilité en croissance réelle.
+                <strong className="text-sunuBlue">SunuLink Consulting</strong> , Nous aidons les entreprises, les institutions et les organisations à
+                développer leur visibilité, renforcer leur image de marque et accélérer
+                leur croissance grâce à des stratégies de communication et de conseil
+                performantes.
               </p>
               <p className="text-lg md:text-xl text-gray-600 leading-relaxed" data-aos="fade-up" data-aos-delay="100">
                 Nous associons stratégie, créativité et technologie pour délivrer des résultats mesurables aux entreprises africaines.
